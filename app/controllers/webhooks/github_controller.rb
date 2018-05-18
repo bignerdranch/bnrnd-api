409ed4a7bb53c5e8e735783318a893265b6314ab
Josh Justice <jjustice@bignerdranch.com>
2018-05-18 12:35:50 -0400

    Set up firebase integration with github

--- app/controllers/webhooks/github_controller.rb
+++ app/controllers/webhooks/github_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'firebase_client'
+
 module Webhooks
   class GithubController < ApplicationController
     CLIENT_ID = ENV['GITHUB_CLIENT_ID']
@@ -14,10 +16,33 @@ module Webhooks
 
         response = HTTParty.get(authd_commits_url)
         num_commits = JSON.parse(response.body).count
-        puts "PR CLOSED WITH #{num_commits} commits"
+        commits_merged(num_commits)
       end
 
       head :no_content
+    end
+
+    private
+
+    def day_path
+      'projectData/sprints/0/days/0'
+    end
+
+    def commits_path
+      "#{day_path}/commits"
+    end
+
+    def firebase
+      @firebase ||= FirebaseClient.create
+    end
+
+    def commits_merged(num_merged_commits)
+      puts 'PR MERGED'
+
+      num_day_commits = firebase.get(commits_path).body
+      response = firebase.update(day_path, { commits: num_day_commits + num_merged_commits })
+      puts response.success?
+      puts response.body
     end
   end
 end
